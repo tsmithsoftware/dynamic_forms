1fe86eb9dcdfb532941170d8681f763bfdbc6c0e
Tim <tijak@protonmail.ch>
2020-12-18 16:26:15 +0000

    update fixture reader to work with flutter test runner

--- test/fixtures/fixture_reader.dart
+++ test/fixtures/fixture_reader.dart
@@ -1,3 +1,13 @@
 import 'dart:io';
+import 'package:path/path.dart' as path;
 
-String fixture(String name) => File('test/fixtures/$name').readAsStringSync();
+// Using Path library to work with Azure DevOps build pipeline (flutter test from project root directory)
+String fixture(String name) {
+  var currentLocation = path.current;
+  if(!(currentLocation.endsWith("test"))){
+    currentLocation = path.join(currentLocation, "test");
+  }
+  var fixtures = path.join(currentLocation, "fixtures");
+  var fullPath = path.join(fixtures, name);
+  return File(fullPath).readAsStringSync();
+}
